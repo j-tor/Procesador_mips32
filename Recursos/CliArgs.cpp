b452329549b9242ff7e06009195b6b5d885faa96
j_tor <alejandrotoso987@gmail.com.com>
2026-06-10 17:30:44 -0600

    feat: refactor CLI arguments to support program, font, and data paths and add gitignore for build artifacts

--- Recursos/CliArgs.cpp
+++ Recursos/CliArgs.cpp
@@ -5,10 +5,12 @@
 void CliArgs::printUsage()
 {
     std::cout
-        << "Usage: " << program << " --file <csv file path>"
+        << "Usage: " << program << " --program <program path> --font <font path> --data <data path>"
         << "\n"
         << "Options:\n"
-        << "  --file <csv file path>          (Required) Specify the CSV file to work with.\n"
+        << "  --program <program path>      (Required) Specify the binary program file in machine language.\n"
+        << "  --font <font path>            (Required) Specify the MIF file with the font.\n"
+        << "  --data <data path>            (Required) Specify the data file.\n"
         << "\n";
 }
 
@@ -17,19 +19,37 @@ std::optional<std::string> CliArgs::parse()
     for (int i = 0; i < argc_; i++) {
         std::string cmd = argv_[i];
 
-        if (cmd == "--file") {
+        if (cmd == "--program") {
             if (i + 1 == argc_) {
-                return std::string("Option '--file' requires a file path");
+                return std::string("Option '--program' requires a file path");
             }
-            filepath_ = argv_[i + 1];
+            programPath_ = argv_[i + 1];
+            i++;
+        } else if (cmd == "--font") {
+            if (i + 1 == argc_) {
+                return std::string("Option '--font' requires a file path");
+            }
+            fontPath_ = argv_[i + 1];
+            i++;
+        } else if (cmd == "--data") {
+            if (i + 1 == argc_) {
+                return std::string("Option '--data' requires a file path");
+            }
+            dataPath_ = argv_[i + 1];
             i++;
         } else {
             return std::format("Invalid option '{}'", cmd);
         }
     }
 
-    if (filepath_.empty()) {
-        return std::string("Option '--file' is required");
+    if (programPath_.empty()) {
+        return std::string("Option '--program' is required");
+    }
+    if (fontPath_.empty()) {
+        return std::string("Option '--font' is required");
+    }
+    if(dataPath_.empty()) {
+        return std::string("Option '--data' is required");
     }
 
     return std::nullopt;
--- Recursos/CliArgs.hpp
+++ Recursos/CliArgs.hpp
@@ -10,8 +10,14 @@ public:
       : argc_(argc - 1), argv_(argv + 1), program(argv[0])
     {}
 
-    std::string filePath() const
-    { return filepath_; }
+    std::string programPath() const
+    { return programPath_; }
+
+    std::string fontPath() const
+    { return fontPath_; }
+
+    std::string dataPath() const
+    { return dataPath_; }
 
     void printUsage();
 
@@ -21,5 +27,7 @@ private:
     int argc_;
     const char **argv_;
     std::string program{};
-    std::string filepath_{};
+    std::string programPath_{};
+    std::string fontPath_{};
+    std::string dataPath_{};
 };
